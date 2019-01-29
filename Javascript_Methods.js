a93c4b8acbc57168ded508508ed7367302f24775
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-01-29 15:54:56 +1100

    js built-in methods

---
+++ Javascript_Methods.js
@@ -0,0 +1,17 @@
+// alert
+// alert is a built-in js method which alerts the user using a pop-up
+alert("Hello There");
+alert(576576);
+alert(198 * 2);
+
+// console.log
+// console.log is a built-in js method which is used to print something on the console
+console.log("Hello from the console");
+
+
+// prompt
+// prompt is a built-in js method which is used to get some input from the user
+var username = prompt("What is your name?");
+
+// To clear the console we use the built-in method called clear()
+clear()
