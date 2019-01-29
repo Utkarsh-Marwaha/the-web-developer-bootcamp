5587631b83d1cced294fed1acef84cfab47d6be1
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-01-29 15:35:17 +1100

    added Javascript variables

--- Javascript_Basics.js
+++ Javascript_Basics.js
@@ -79,3 +79,27 @@ undefined
 
 
 
+// Variables
+
+// Variables are simply containers that store values
+// They follow this pattern:
+var yourVariableName = "yourValue" // does not have to be a string
+
+// They can store all of the values we've seen
+var name = "Rusty";
+var secretNumber = 73;
+var isAdorable = true;
+
+// Recall the stored value by calling the variable name
+var name = "Rusty";
+"hello there " + name
+
+var num = 37;
+num + 3 + 10     //50
+
+//We can also update existing variables
+var name = "Robert";
+name = "Bob";
+
+// Javascript is dynamically typed which means we can change the data type of the value stores in the variables
+secretNumber = "Seven Two One";
