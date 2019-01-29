79b0514b81c40423eaa944f67621b2b6fa94ca8e
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-01-29 16:11:07 +1100

    added JS built-in methods exercise

---
+++ scriptsExercise.js
@@ -0,0 +1,10 @@
+
+// ask for user's details
+var firstName = prompt("What is your first name?");
+var lastName  = prompt("What is your last name?");
+var age		  = prompt("How old are you?");
+
+
+// print out the user's details on the console
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
