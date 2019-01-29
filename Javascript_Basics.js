b9e5247bd0ca332696719c7cee43057d1d561b12
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-01-29 15:34:49 +1100

    started javascript basics

---
+++ Javascript_Basics.js
@@ -0,0 +1,81 @@
+// Objectives
+
+// 1. Introduce the 5 primitive data types
+// 2. Work with numbers and numeric operators
+// 3. Work with strings and common string methods
+
+// 5 Primitive Datatypes
+
+// Numbers
+4
+9.3
+-10
+// Javascript treats all numbers be it positive, negative, decimal or fractional as same
+
+//Strings
+"Hello World"
+"43"
+
+// Booleans
+true
+false
+
+// null and undefined
+null
+undefined
+
+
+
+// Numbers
+4
+9.3
+-10
+
+// We can do mathematical operations with them
+
+4 + 10	//14
+1/5		//0.2
+
+// Modulo - remainder operator
+
+10 % 3	//1
+24 % 2	//0
+15 % 11 //4
+
+
+// Single or Double quotes OK
+"hello world"
+"hello world"
+
+// Concatenation
+"charlie" + "brown" // charliebrown
+
+// Escape characters start with "\"
+"Signin \"Do wah diddy, dum diddy do\""
+"This is a backslash \\"
+
+//Strings have a length property
+"hello world".length //11
+
+//Access individual characters using [] and an index
+"hello"[0] //"h"
+"hello"[4] //"o"
+"hello"["hello".length-1]
+
+// Exercise for Javascript Basics
+
+// Evaluate the following statements without executing them on the browser
+// 1.
+100 % 3 
+// Answer: 1
+
+// 2.
+("blah" + "blah")[6]
+// Answer: "a"
+
+// 3.
+"hello".length % "hi\\".length
+// Answer: 5 % 3 = 2
+
+
+
